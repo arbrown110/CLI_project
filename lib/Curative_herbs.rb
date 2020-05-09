b4da00fdef4f5ab6ae20c59dd551f45ff976a23d
arbrown110 <arbrown110@gmail.com>
2020-05-08 23:20:12 -0400

    added first step for scraping.

--- lib/Curative_herbs.rb
+++ lib/Curative_herbs.rb
@@ -1,7 +1,8 @@
 require 'open-url'
 require 'nokogiri'
 require 'pry'
+require 'HTTParty'
   
-require_ relative "./Curative_herbs/cli"
-require_ relative "./Curative_herbs/herbs"
+require_relative "./Curative_herbs/cli"
+require_relative "./Curative_herbs/herbs"
 require_relative "./Curative_herbs/version"
--- lib/Curative_herbs/herbs.rb
+++ lib/Curative_herbs/herbs.rb
@@ -1,14 +1,28 @@
+require 'nokogiri'
+require 'pry'
+require 'HTTParty'
+
+
 class CurativeHerbs::Herbs
  attr_accessor :name, :history, :learn
 
 
- def self.knowledge
+  def self.knowledge
     #there should be alot of instances of herbs
   self.scrape_herbs
- end
- def self.scrape_herbs
-  herbs =[] #kept empty
-  [the different herb & properties]
-  herbs
- end
-end 
+  end
+
+
+ 
+  def self.scrape_remedies
+    herb =[]
+    #gather all info , extract ,and present
+    herb << self.scrape_medline
+    herb
+  end 
+  
+  def self.scrape_medline
+  doc = Nokogiri::HTML(open("https://medlineplus.gov/druginfo/herb_All.html"))
+    #call CurativeHerbs::Herbs.scrape_medline  to check as you go in ./bin/console 
+  end
+end
